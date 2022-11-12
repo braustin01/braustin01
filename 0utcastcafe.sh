50648d341008a67a9da4afa45e1996d67647596b
braustin01 <111617757+braustin01@users.noreply.github.com>
2022-11-12 13:47:34 -0800

    Having fun with directory. Linux.
    I'm trying to have fun exploring code with the bash script. Creating new directory with the bash script is fun!

---
+++ 0utcastcafe.sh
@@ -0,0 +1,16 @@
+
+
+    #!/bin/bash
+    # A sample Bash script, by Blair
+    echo ------------------------
+    echo ----- Outcastcafe-------
+    echo ------------------------
+    echo ------------------------
+    echo $PATH
+    echo /home/outcast/
+    echo outcast@bash
+    touch /home/outcast/0utcastcafe.txt 
+    touch /home/outcast/0utcastcafe2.txt
+    touch /home/outcast/0utcastcafe3.txt
+    touch /home/outcast/0utcastcafe4.txt
+    touch /home/outcast/0utcastcafe5.txt
